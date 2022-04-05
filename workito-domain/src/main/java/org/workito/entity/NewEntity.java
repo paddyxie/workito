22f3af465a11dfad4cc0e35f0cdd83f8d7ac92e1
Paddy <xpjsky@gmail.com>
2022-04-05 14:11:13 +0800

    project init

---
+++ workito-domain/src/main/java/org/workito/entity/NewEntity.java
@@ -0,0 +1,11 @@
+package org.workito.entity;
+
+import org.springframework.data.jpa.domain.AbstractAuditable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+//@Entity
+//@Table(name = "new_entity")
+public class NewEntity extends AbstractAuditable {
+}
